721d51372b9998026844c14b6e3732f6c7379ddd
chaikivskti.t <chaikivskyi.t@gmail.com>
2026-01-11 01:59:21 +0100

    update name file

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from "./contactsSlice";
+import { contactsReducer } from "./contactsOps";
 import { filtersReducer } from "./filtersSlice";
 
 export const store = configureStore({
